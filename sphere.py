c9a80e3eff0755ab02bdfe6c3ab4f7210dd769b2
Ubuntu <ubuntu@ip-172-31-80-220.ec2.internal>
2022-04-01 01:54:34 +0000

    finished sphere.py

--- sphere.py
+++ sphere.py
@@ -3,24 +3,26 @@
 # March 30, 2022
 # This program calculates the surface area and volume of a sphere
 import math
+import colorama
+from colorama import Fore
 
 
 def main():
-    # input
-    print("Today we will calculate the surface area and"\
+    # get the radius and units to calculate for the surface area and volume
+    print(Fore.RED + "Today we will calculate the surface area and"
     " volume of a sphere")
     print("")
-    units = input("What units are you using? (mm, cm, etc.): ")
-    radius = int(input("What is the radius of your sphere ({}) :"  .format(units)))
+    units = input(Fore.YELLOW + "What units are you using? (mm, cm, etc.): ")
+    radius = float(input("What is the radius of your sphere ({}): " .format(units)))
 
-    # the process
+    # calculate the surface area and volume of the sphere
     surface_area = 4 * math.pi * radius**2
     volume = 4/3 * math.pi * radius**3
 
-
     # Display the surface area and volume to the user
     print("")
-    print("The surface area of the sphere is: {:.2f} {}^2" .format(surface_area, units))
+    print(Fore.BLUE + "The surface area of the sphere is: {:.2f} {}^2" 
+    .format(surface_area, units))
     print("The volume is: {:.2f} {}^3" .format(volume, units))
 
 
